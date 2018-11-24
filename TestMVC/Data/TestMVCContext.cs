0384d6a0d6485835de6562cba51ec496cf9f84b8
hanhhg0607 <hanhgtd00593@fpt.edu.vn>
2018-11-24 10:21:51 +0700

    nop_bai

---
+++ TestMVC/Data/TestMVCContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TestMVC.Models;
+
+namespace TestMVC.Models
+{
+    public class TestMVCContext : DbContext
+    {
+        public TestMVCContext (DbContextOptions<TestMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TestMVC.Models.Product> Product { get; set; }
+
+        public DbSet<TestMVC.Models.Category> Category { get; set; }
+    }
+}
---
+++ TestMVC/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestMVC.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int SupplierID { get; set; }
+        public int CategoryID { get; set; }
+       
+        public Category Category { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public string UnitPrice { get; set; }
+        public string UnitslnStocks { get; set; }
+        public string UnitsOnOrder { get; set; }
+        public string ReorderLevel { get; set; }
+        public string Discontinued { get; set; }
+       
+
+    }
+}
